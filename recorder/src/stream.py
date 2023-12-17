d17143ba124711480af011b2d8df519b17fd6f80
yazdani kiyan <yazdani.kiyan@proton.me>
2023-12-17 18:10:51 +0100

    chore(recorder): do not fail when rabbitmq is not running
    Avoid crashing when rabbitmq is not running. This will only disable the
    notifications feature.

--- recorder/src/stream.py
+++ recorder/src/stream.py
@@ -11,15 +11,20 @@ SOCKET_LOCATION = env.get("AUREC_STREAM_SOCKET") or "/tmp/aurec.stream.socket"
 
 class Stream():
     def __init__(self):
-        self.conn = pika.BlockingConnection(pika.ConnectionParameters(HOST))
-        self.channel = self.conn.channel()
-        self.channel.queue_declare(queue=NOTIFICATION_QUEUE)
+        try:
+            self.conn = pika.BlockingConnection(pika.ConnectionParameters(HOST))
+            self.channel = self.conn.channel()
+            self.channel.queue_declare(queue=NOTIFICATION_QUEUE)
+        except:
+            self.conn = None
+            print("Could not connect to Rabbitmq: notifications are disabled")
 
     def send_frame(self, frame: Frame):
         with open(SOCKET_LOCATION, "wb") as f:
             f.write(frame)
             f.close()
-        
+
     def send_message(self, kind: str, data: Any):
-        body = kind + " | " + data
-        self.channel.basic_publish(exchange='amq.direct', routing_key=NOTIFICATION_QUEUE, body=body)
+        if self.conn is not None:
+            body = kind + " | " + data
+            self.channel.basic_publish(exchange='amq.direct', routing_key=NOTIFICATION_QUEUE, body=body)
