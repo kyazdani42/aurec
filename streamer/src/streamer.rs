ff490d4e22ecffabd3fdc1139d2aa72c737c9d0e
yazdani kiyan <yazdani.kiyan@proton.me>
2023-12-25 18:10:22 +0100

    feat(streamer): implement stream pending and stream end
    Implement stream pending by calling the waker after a 20ms pause. This
    makes possible to have a 60fps stream and avoid 0 bytes read files to be
    sent.
    Implement stream ending when more than 10mb are read. This allows the
    stream to not be infinite and finish properly on the caller side.

--- streamer/src/streamer.rs
+++ streamer/src/streamer.rs
@@ -1,4 +1,9 @@
-use actix_web::{web::Bytes, Error};
+use actix_web::{
+    rt::{spawn, time},
+    web::Bytes,
+    Error,
+};
+use futures::task::Waker;
 use futures::Stream;
 
 use std::{
@@ -6,6 +11,7 @@ use std::{
     fs::read,
     pin::Pin,
     task::{Context, Poll},
+    time::Duration,
 };
 
 fn get_socket_location() -> Result<String, String> {
@@ -14,21 +20,54 @@ fn get_socket_location() -> Result<String, String> {
 
 pub struct Streamer {
     socket: String,
+    bytes_read: usize,
+    wait_next: bool,
 }
 
 impl Streamer {
     pub fn new() -> Result<Self, String> {
         Ok(Streamer {
             socket: get_socket_location()?,
+            bytes_read: 0,
+            wait_next: false,
         })
     }
+
+    fn wake_later(&self, waker: &Waker) {
+        let waker = waker.clone();
+        spawn(async move {
+            time::sleep(Duration::from_millis(20)).await;
+            waker.wake_by_ref();
+        });
+    }
 }
+
+const MAX_BYTES_READ: usize = 10_000_000;
 
 impl Stream for Streamer {
     type Item = Result<Bytes, Error>;
 
-    fn poll_next(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        if self.bytes_read > MAX_BYTES_READ {
+            return Poll::Ready(None);
+        }
+
+        if self.wait_next {
+            self.wake_later(cx.waker());
+            self.get_mut().wait_next = false;
+            return Poll::Pending;
+        }
+
         if let Ok(f) = read(&self.socket) {
+            if f.len() == 0 {
+                self.wake_later(cx.waker());
+                return Poll::Pending;
+            }
+
+            let this = self.get_mut();
+            this.bytes_read = this.bytes_read + f.len();
+            this.wait_next = true;
+
             Poll::Ready(Some(Ok(Bytes::from_iter(f))))
         } else {
             Poll::Ready(None)
